6e127044ca479283b484d12e81fb51f679a730f2
Zum-Whol <77596208+Zum-Whol@users.noreply.github.com>
2021-11-08 15:03:23 +0900

    Create LandingPage.js

---
+++ client/src/pages/LandingPage.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+import Map from "./Map";
+import "./LandingPage_CSS/LandingPage.css";
+import NavBar from "../components/NavBar.js";
+import Footer from "../components/Footer.js";
+import TopBtn from "../components/TopBtn.js";
+import Feature from "../components/Feature.js";
+import Service from "../components/Service";
+
+function LandingPage() {
+  return (
+    <>
+      <TopBtn />
+      <NavBar />
+      <div style={{ overflow: "hidden" }}>
+        <Service />
+        <Feature />
+        <Footer />
+      </div>
+    </>
+  );
+}
+export default LandingPage;
