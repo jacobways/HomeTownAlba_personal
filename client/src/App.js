7f918ef2163e698697023e59577f00c636a6f749
jacobways <80403988+jacobways@users.noreply.github.com>
2021-11-11 15:09:54 +0900

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import RegisterPage from "./pages/RegisterPage";
 import CompanyMyPage from "./pages/CompanyMyPage";
 import JobSeekerMyPage from "./pages/JobSeekerMyPage";
 import LandingPage from "./pages/LandingPage.js";
+import Error from "./pages/ErrorPage";
 
 import Loading from "./components/Loading/Loading";
 import Map from "./pages/Map";
@@ -15,17 +16,31 @@ import MyPage from "./pages/MyPage";
 import ChatPage from "./components/ChatPage";
 
 function App() {
+  
+  const [guestApplyStatus, setGuestApplyStatus] = useState(false)
+
+  const guestApplyStatusHandler = () => {
+    setGuestApplyStatus(!guestApplyStatus)
+  }
+  
   return (
     <>
       <Switch>
         <Route exact path="/" component={LandingPage} />
         <Route exact path="/mypage" component={MyPage} />
-        <Route exact path="/map" component={Map} />
+        <Route exact path="/map" 
+         render={()=><Map guestApplyStatus={guestApplyStatus} 
+         guestApplyStatusHandler={guestApplyStatusHandler}/>}
+        />
         <Route exact path="/admin" component={AdminPage} />
-        <Route exact path="/guest/mypage" component={GuestMyPage} />
+        <Route exact path="/guest/mypage" 
+         render={()=><GuestMyPage 
+         guestApplyStatus={guestApplyStatus}/>} 
+        />
         <Route exact path="/login" component={LoginPage} />
         <Route exact path="/register" component={RegisterPage} />
         <Route exact path="/chat" component={ChatPage} />
+        <Route path="*" component={Error} />
       </Switch>
     </>
   );
