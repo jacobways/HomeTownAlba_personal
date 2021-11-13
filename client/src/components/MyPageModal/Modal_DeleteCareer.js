72b9df5186f6a84f4f9e8374a5c693333192988b
kmh0818 <85830779+kmh0818@users.noreply.github.com>
2021-11-13 23:07:13 +0900

    Update Modal_DeleteCareer.js

--- client/src/components/MyPageModal/Modal_DeleteCareer.js
+++ client/src/components/MyPageModal/Modal_DeleteCareer.js
@@ -4,14 +4,14 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
 export const ModalBackdrop = styled.div`
-  position: fixed;
+  
   z-index: 999;
   top: 1;
   left: 1;
   bottom: 1;
   right: 1;
   background-color: rgba(0, 0, 0, 0.4);
-  display: grid;
+  display: -webkit-flex;
   place-items: center;
 `;
 
