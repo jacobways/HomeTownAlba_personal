e26b57c748aa7c13b80f575a710c21326db35073
Zum-Whol <77596208+Zum-Whol@users.noreply.github.com>
2021-11-14 01:19:56 +0900

    텍스트 변경

--- client/src/components/LoadingModal.js
+++ client/src/components/LoadingModal.js
@@ -3,7 +3,24 @@ import React from "react";
 function LoadingModal() {
   return (
     <>
-      <div>인증메일이 도착할때까지 잠시만 기다려주세요</div>
+      <div
+        style={{
+          fontSize: "1rem",
+          marginBottom: "0px",
+          fontWeight: 900,
+        }}
+      >
+        인증메일이 도착할때까지
+      </div>
+      <div
+        style={{
+          fontSize: "1rem",
+          marginBottom: "0px",
+          fontWeight: 900,
+        }}
+      >
+        잠시만 기다려주세요
+      </div>
     </>
   );
 }
